14a99a5440aec7bcd4eadd63646d9b593fbdd7bd
Ivan Kipyegon <ivankp1@gmail.com>
2019-06-28 16:21:35 +0300

    feat(): generate mongo url with environment variables

--- config/development-local.js
+++ config/development-local.js
@@ -1,9 +1,17 @@
+const {
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_HOSTNAME,
+  MONGO_PORT,
+  MONGO_DB,
+} = process.env;
+
 module.exports = {
   app: {
     httpsEnabled: false,
   },
   mongo: {
-    uri: 'mongodb://db:27017/survey',
+    uri: `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`,
     debug: true,
   },
   jwt: {
--- local/dev-env.sh
+++ local/dev-env.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export JWT_KEY='4xL1e0ren2SOyN1rNJpR12CCeObcy69'
 export PORT=8080
-# export NODE_ENV='development-local'
+export NODE_ENV='development-local'
 export SALT_WORK_FACTOR=1
 export APP_NAME="KOA API"
-export MONGO_USERNAME=ivantoz
+export MONGO_USERNAME=admin
 export MONGO_PASSWORD=test
 export MONGO_HOSTNAME=db
 export MONGO_PORT=27017
